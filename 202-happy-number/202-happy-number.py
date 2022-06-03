46fd4156b48010f2f7e94a2b425cb32ccf94abb0
Alex Sorokyn <sorokin.a.n.post@gmail.com>
2022-06-03 11:23:26 +0300

    Time: 55 ms (34.41%), Space: 13.9 MB (59.77%) - LeetHub

--- 202-happy-number/202-happy-number.py
+++ 202-happy-number/202-happy-number.py
@@ -4,8 +4,17 @@ class Solution:
         sum_all_digital = n
         while sum_all_digital not in prev_sums:
             prev_sums.add(sum_all_digital)
-            sum_all_digital = sum(map(lambda x: int(x)**2, list(str(sum_all_digital))))
-
+            sum_all_digital = self.sum_square(sum_all_digital)
             if sum_all_digital == 1:
                 return True
         return False
+
+    def sum_square(self, n: int) -> int:
+        output = 0
+
+        while n:
+            div, mod = divmod(n, 10)
+            output += mod**2
+            n = div
+
+        return output
