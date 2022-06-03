4ff99eae50c577db21e80d3a7396db0aabe7af22
Alex Sorokyn <sorokin.a.n.post@gmail.com>
2022-06-03 11:18:16 +0300

    Time: 70 ms (11.04%), Space: 13.9 MB (59.77%) - LeetHub

--- 202-happy-number/202-happy-number.py
+++ 202-happy-number/202-happy-number.py
@@ -1,9 +1,11 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
+        prev_sums = set()
         sum_all_digital = n
-        for i in range(100):
+        while sum_all_digital not in prev_sums:
+            prev_sums.add(sum_all_digital)
             sum_all_digital = sum(map(lambda x: int(x)**2, list(str(sum_all_digital))))
-            print(sum_all_digital)
+
             if sum_all_digital == 1:
                 return True
         return False
